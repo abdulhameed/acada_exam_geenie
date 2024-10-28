ab6d92736cc88a32c7285b4e3bf1b8149d789cd9
abdulhameed <abdulhameedgiwa@gmail.com>
2024-10-28 21:12:16 +0100

    add secret key fallback

--- exam_geenie/settings.py
+++ exam_geenie/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from celery import Celery
-from decouple import config, Csv
+from decouple import config, Csv, UndefinedValueError
 # from celery.schedules import crontab
 # import djcelery
 
@@ -29,7 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+
+try:
+    SECRET_KEY = config('SECRET_KEY')
+except UndefinedValueError:
+    SECRET_KEY = 'fallback-secret-key-for-local-dev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
