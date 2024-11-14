1a18f205d757f269d7af7cf67cefe0cf847428d4
Abdul <abdulhameedgiwa@gmail.com>
2024-11-14 17:04:47 +0100

    Update settings.py
    same as last commit

--- exam_geenie/settings.py
+++ exam_geenie/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,exam-geenie-avegdeesaufpbmdp.northeurope-01.azurewebsites.net,examgenie.online', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,examgenie.online', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = ['https://exam-geenie-avegdeesaufpbmdp.northeurope-01.azurewebsites.net']
 
